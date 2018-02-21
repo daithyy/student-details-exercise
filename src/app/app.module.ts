6e9f93ee213c7c737d5365368e7def4f55ee2539
David Brennan <david.brennan@mail.itsligo.ie>
2018-02-21 12:47:02 +0000

    Added detail component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { DetailComponent } from './detail/detail.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule
