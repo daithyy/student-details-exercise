92fc9b484bb368c40c5ed00e7352210dbd112449
David Brennan <david.brennan@mail.itsligo.ie>
2018-02-22 14:01:55 +0000

    Added CSS and detail array with detail objects.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Detail } from './detail/detail.model'; 
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  Detail : Detail[];
+
+  constructor() {
+    this.Detail = [
+    new Detail(
+      "David Brennan", "52 Brooklyn Avenue", 
+      "S00172994", "044 9234728", "Games Developer",
+      ".images/user.jpg"),
+    new Detail(
+      "Adrian Fearon", "10 Oakcrest Rd.", 
+      "S00148923", "082 6284657", "Lead Audio Artist",
+      ".images/user.jpg"), 
+    new Detail(
+      "Jack Gilmartin", "36 Mellow Street", "S00182481", 
+      "086 3159035", "Lead Engine Programmer",
+      ".images/user.jpg"),
+    new Detail(
+      "Liam McManus", "9 Fairview", 
+      "S00169401", "089 4139502", "Game Designer",
+      ".images/user.jpg")]
+  }
 }
--- src/app/detail/detail.model.ts
+++ src/app/detail/detail.model.ts
@@ -4,14 +4,16 @@ export class Detail {
     studentID: string;
     phoneNumber: string;
     notes: string;
+    image: string;
 
     constructor(name: string, address: string, studentID: string,
-                phoneNumber: string, notes: string)
+                phoneNumber: string, notes: string, image: string)
     {
         this.name = name;
         this.address = address;
         this.studentID = studentID;
         this.phoneNumber = phoneNumber;
         this.notes = notes;
+        this.image = image;
     }
 }
